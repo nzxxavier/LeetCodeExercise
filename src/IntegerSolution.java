0ce1d353226af2c0d7bbbf91bb576192551af9d7
nzxxavier <15927400635@163.com>
2020-02-20 15:14:12 +0800

    三数之和

--- src/IntegerSolution.java
+++ src/IntegerSolution.java
@@ -1,6 +1,5 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class IntegerSolution {
     public static int maxArea(int[] height) {
@@ -54,6 +53,30 @@ public class IntegerSolution {
             }
         }
 
+        return res;
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for(int k = 0; k < nums.length - 2; k++){
+            if(nums[k] > 0) break;
+            if(k > 0 && nums[k] == nums[k - 1]) continue;
+            int i = k + 1, j = nums.length - 1;
+            while(i < j){
+                int sum = nums[k] + nums[i] + nums[j];
+                if(sum < 0){
+                    while (i < j && nums[i] == nums[++i]);
+                } else if (sum > 0) {
+                    while (i < j && nums[j] == nums[--j]);
+                } else {
+                    res.add(new ArrayList<Integer>(Arrays.asList(nums[k], nums[i], nums[j])));
+                    while (i < j && nums[i] == nums[++i]);
+                    while (i < j && nums[j] == nums[--j]);
+                }
+            }
+        }
         return res;
     }
 }
