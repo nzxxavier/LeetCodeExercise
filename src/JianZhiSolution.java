2311ad109e5118eb1b7c254dc7b90677bd362e27
nzxxavier <15927400635@163.com>
2020-02-23 17:40:47 +0800

    剑指Offer好多题

--- src/JianZhiSolution.java
+++ src/JianZhiSolution.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class JianZhiSolution {
     public static int findRepeatNumber(int[] nums) {
         int[] mem = new int[nums.length];
@@ -11,5 +15,60 @@ public class JianZhiSolution {
             mem[nums[i]] ++;
         }
         return 0;
+    }
+
+    public static boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int row = 0, column = columns - 1;
+        while (row < rows && column >= 0) {
+            int num = matrix[row][column];
+            if (num == target) {
+                return true;
+            } else if (num > target) {
+                column--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+
+    public static String replaceSpace(String s) {
+        StringBuilder builder = new StringBuilder();
+        int len = s.length();
+        for (int i = 0; i < len; i ++) {
+            if (s.charAt(i) == ' ')
+                builder.append("%20");
+            else
+                builder.append(s.charAt(i));
+        }
+        return builder.toString();
+    }
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+    }
+
+    public int[] reversePrint(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        if (head == null) {
+            return new int[0];
+        }
+        stack.push(head.val);
+        while (head.next != null) {
+            head = head.next;
+            stack.push(head.val);
+        }
+        int len = stack.size();
+        int[] res = new int[len];
+        for (int i = 0; i < len; i ++) {
+            res[i] = stack.pop();
+        }
+
+        return res;
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        int[] nums = {2, 3, 1, 0, 2, 5, 3};
-        System.out.println(JianZhiSolution.findRepeatNumber(nums));
+        int[][] nums = {{1,4,7,11,15}, {2,5,8,12,19}, {3,6,9,16,22}, {10,13,14,17,24}, {18,21,23,26,30}};
+        int[][] num1 = {{}};
+        System.out.println(JianZhiSolution.findNumberIn2DArray(nums, 5));
     }
 }
