b003cbec3ef5442648d5cc44a7b89e73e6ab85cf
nzxxavier <15927400635@163.com>
2020-03-06 15:54:21 +0800

    面试题10- I. 斐波那契数列

--- src/JianZhiSolution.java
+++ src/JianZhiSolution.java
@@ -124,4 +124,14 @@ public class JianZhiSolution {
         else
             return null;
     }
+
+    public static int fib(int n) {
+        int a = 0, b = 1, sum;
+        for(int i = 0; i < n; i++){
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
 }
