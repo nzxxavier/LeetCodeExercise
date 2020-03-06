f4e28315270ef362f91a475e79ca3732a4caddcd
nzxxavier <15927400635@163.com>
2020-03-06 16:48:26 +0800

    面试题12. 矩阵中的路径

--- src/JianZhiSolution.java
+++ src/JianZhiSolution.java
@@ -1,8 +1,3 @@
-import com.sun.org.apache.xalan.internal.xsltc.dom.SingleNodeCounter;
-import sun.reflect.generics.tree.Tree;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 public class JianZhiSolution {
@@ -133,5 +128,45 @@ public class JianZhiSolution {
             b = sum;
         }
         return a;
+    }
+
+    public static int minArray(int[] numbers) {
+        int min = Integer.MAX_VALUE;
+        int count = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+                count = i;
+            }
+        }
+
+        int[] res = new int[numbers.length];
+        for(int i = 0; i < numbers.length; i++){
+            res[i] = numbers[count % (numbers.length)];
+            count ++;
+        }
+        return res[0];
+    }
+
+    public static boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(dfs(board, words, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+    private static boolean dfs(char[][] board, char[] word, int i, int j, int k) {
+        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) return false;
+        if(k == word.length - 1) return true;
+        char tmp = board[i][j];
+        board[i][j] = '#';
+        boolean res = dfs(board, word, i + 1, j, k + 1) ||
+                dfs(board, word, i - 1, j, k + 1) ||
+                dfs(board, word, i, j + 1, k + 1) ||
+                dfs(board, word, i , j - 1, k + 1);
+        board[i][j] = tmp;
+        return res;
     }
 }
--- src/main.java
+++ src/main.java
@@ -3,8 +3,7 @@ public class main {
         //int[][] nums = {{1,4,7,11,15}, {2,5,8,12,19}, {3,6,9,16,22}, {10,13,14,17,24}, {18,21,23,26,30}};
         //int[][] num1 = {{}};
         //System.out.println(JianZhiSolution.findNumberIn2DArray(nums, 5));
-        int[] pre = {-1};
-        int[] in = {-1};
-        System.out.println(JianZhiSolution.buildTree(pre, in));
+        char[][] board = {{'a','b','c','e'}, {'s','f','c','s'}, {'a','d','e','e'}};
+        System.out.println(JianZhiSolution.exist(board,"abcced"));
     }
 }
